671535afb7a4cbca58d330b1a723c59a4424b73e
Marcel-Patrick <90202110+Marcel-Patrick@users.noreply.github.com>
2022-02-21 23:58:44 +0100

    Update movie card view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // import routing
 import { RouterModule, Routes } from '@angular/router';
@@ -71,6 +72,7 @@ const appRoutes: Routes = [
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    FlexLayoutModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [],
